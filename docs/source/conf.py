1308b17bc99e50db75c4c24c48d133c1cec4f869
Qian Cao <144297473+QianC95@users.noreply.github.com>
2024-02-14 11:37:09 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'LocationEncoder'
+copyright = '2024, SEAI'
+author = 'SEAI'
 
 release = '0.1'
 version = '0.1.0'
